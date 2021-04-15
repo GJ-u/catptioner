93c5f73919ea8644ecf5ce3fb8fdd7a73e9b7fae
Grovyle/Justice <79778413+GJ-u@users.noreply.github.com>
2021-04-15 20:56:36 +1000

    add image size text while making edit

--- catptioner.py
+++ catptioner.py
@@ -24,12 +24,16 @@ def drawText():
     img = Image.open(getFilePath())
     draw = ImageDraw.Draw(img)
     text = getFullCaption()
-    font = ImageFont.truetype("cinecavDmono.ttf", 150)
+    font = ImageFont.truetype("cinecavDmono.ttf", 19)
 
     # size variables
-    x, y = (200, 2750)
+    x, y = (20, 430)
     w, h = font.getsize(text)
 
+    imgw, imgh = img.size
+
+    # confirm
+    print("The image is " + str(imgw) + "x" + str(imgh) + "\nThe recommended size is 710x473.")
     # edit the image
     draw.rectangle((x, y, x + w, y + h), fill='black')
     draw.text((x, y), text, fill=(250, 250, 250), font=font)
