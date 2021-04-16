f2b03309de58aaeb937a784527d1d52e7f545247
Grovyle/Justice <79778413+GJ-u@users.noreply.github.com>
2021-04-16 22:41:54 +1000

    add image resize

--- catptioner.py
+++ catptioner.py
@@ -4,8 +4,8 @@ from PIL import Image, ImageFont, ImageDraw
 def getFullCaption():
     inputTone = input("Tone: ")
     tone = ("[ " + inputTone.upper() + " ] ")
-    captiontext = input("Caption: ")
-    fullCaption = tone + captiontext
+    captionText = input("Caption: ")
+    fullCaption = tone + captionText
     return fullCaption
 
 
@@ -19,26 +19,32 @@ def getOutputName():
     return outputName
 
 
+
 def drawText():
     # appearance variables
     img = Image.open(getFilePath())
-    draw = ImageDraw.Draw(img)
-    text = getFullCaption()
     font = ImageFont.truetype("cinecavDmono.ttf", 19)
 
     # size variables
     x, y = (20, 430)
-    w, h = font.getsize(text)
-
     imgw, imgh = img.size
 
-    # confirm
-    print("The image is " + str(imgw) + "x" + str(imgh) + "\nThe recommended size is 710x473.")
-    # edit the image
-    draw.rectangle((x, y, x + w, y + h), fill='black')
-    draw.text((x, y), text, fill=(250, 250, 250), font=font)
-    img.save(getOutputName() + '.jpg')
+    # image resizing
+    if (imgw, imgh) == (710, 473):
+        print("The image is the recommended size.")
+    else:
+        print("The image is " + str(imgw) + "x" + str(imgh) + "\nResizing image to 710x473.")
+        img = img.resize((710, 473), Image.ANTIALIAS)
 
-    # done
+    # text + background variables
+    text = getFullCaption()
+    w, h = font.getsize(text)
+
+    # edit the image
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((x, y, x + w, y + h), fill="black")
+    draw.text((x, y), text, fill="white", font=font)
+    img.save(getOutputName() + ".jpg")
+
 
 drawText()
